77ce321eeaa63a107b493c2c15b469ef0992a641
Baha2r <baha2r.mirzazadeh98@gmail.com>
2022-05-03 22:35:04 +0430

    working on feature extraction techniques

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import pandas as pd
 import seaborn as sb
 import matplotlib.pyplot as plt
 from sklearn.feature_selection import SelectKBest, chi2
-from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix, classification_report
 
 X_train = None
 X_test = None
@@ -12,33 +15,104 @@ y_train = None
 y_test = None
 
 
-def read_dataset() -> dict:
+def read_dataset():
     return {'data': pd.read_csv('dataset/data.csv').drop(columns=['Unnamed: 0']),
             'labels': pd.read_csv('dataset/labels.csv').drop(columns=['Unnamed: 0'])}
 
 
-def dataset_splitter() -> None:
+def feature_selector():
     dataset = read_dataset()
+    X = dataset['data']
+    y = dataset['labels']
+    feature_scores = pd.concat(
+        [pd.DataFrame(X.columns), pd.DataFrame(SelectKBest(score_func=chi2, k=25).fit(X, y).scores_)],
+        axis=1)
+    feature_scores.columns = ['Features', 'Score']
+    best_features = X[feature_scores.nlargest(25, 'Score')['Features'].values]
     global X_train, X_test, y_train, y_test
-    X_train, X_test, y_train, y_test = train_test_split(dataset['data'], dataset['labels'], test_size=0.2,
+    X_train, X_test, y_train, y_test = train_test_split(best_features.values, y.values.reshape(801, ), test_size=0.2,
                                                         random_state=42)
+    # df = pd.DataFrame(y_test)
+    # df.at[0, 0] = 'BRCA'
+    # y_test = df.values.reshape(161, )
+    # print(y_test)
     return
 
 
-def feature_selector() -> None:
-    # apply SelectKBest class to extract top 10 best features
-    dataset = read_dataset()
-    X = dataset['data']
-    # y = LabelEncoder().fit_transform(dataset['labels'])
-    y = dataset['labels']
-    best_features = SelectKBest(score_func=chi2, k=10)
-    scores = pd.DataFrame(best_features.fit(X, y).scores_)
-    columns = pd.DataFrame(X.columns)
-    featureScores = pd.concat([columns, scores], axis=1)
-    featureScores.columns = ['Specs', 'Score']
-    print(featureScores.nlargest(10, 'Score'))
+# def feature_extractor():
+#     dataset = read_dataset()
+#     X = dataset['data'].values
+#     y = dataset['labels'].values.reshape(801, )
+#     lda = LDA()
+#     lda.fit(X, y)
+#     return lda.transform(X).shape
+
+
+def normalizer():
+    return {'scaled_X_train': MinMaxScaler().fit_transform(X_train, y_train),
+            'scaled_X_test': MinMaxScaler().fit_transform(X_test, y_test)}
+
+
+def fit_train_predict():
+    print('Logistic Regression')
+    lr = LogisticRegression(solver='saga')
+    lr.fit(X_train, y_train)
+    y_pred_test = lr.predict(X_test)
+    print('Test confusion matrix:')
+    cm = confusion_matrix(y_test, y_pred_test)
+    print(cm)
+    print()
+    print('Accuracy:')
+    print(cm.trace() / cm.sum())
+    print()
+    print('Classification report:')
+    print(classification_report(y_test, y_pred_test))
+    print()
+    y_pred_train = lr.predict(X_train)
+    print('Train confusion matrix:')
+    cm = confusion_matrix(y_train, y_pred_train)
+    print(cm)
+    print()
+    print('Accuracy:')
+    print(cm.trace() / cm.sum())
+    print()
+    print('Classification report:')
+    print(classification_report(y_train, y_pred_train))
+
+    print()
+    print()
+
+    print('Gaussian Naive Bayes')
+    gnb = GaussianNB()
+    gnb.fit(X_train, y_train)
+    y_pred_test = lr.predict(X_test)
+    print('Test confusion matrix:')
+    cm = confusion_matrix(y_test, y_pred_test)
+    print(cm)
+    print()
+    print('Accuracy:')
+    print(cm.trace() / cm.sum())
+    print()
+    print('Classification report:')
+    print(classification_report(y_test, y_pred_test))
+    print()
+    y_pred_train = lr.predict(X_train)
+    print('Train confusion matrix:')
+    cm = confusion_matrix(y_train, y_pred_train)
+    print(cm)
+    print()
+    print('Accuracy:')
+    print(cm.trace() / cm.sum())
+    print()
+    print('Classification report:')
+    print(classification_report(y_train, y_pred_train))
     return
 
 
 if __name__ == '__main__':
+    # print(feature_extractor())
     feature_selector()
+    scaled_features = normalizer()
+    X_train = scaled_features['scaled_X_train']
+    X_test = scaled_features['scaled_X_test']
+    fit_train_predict()
